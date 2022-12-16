b6a3f49cdc8161fae0cee1dfd84fbeee1bfccbf1
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-12-16 20:38:01 +0100

    OU Structure

---
+++ Private/New-OUStructure.ps1
@@ -0,0 +1,56 @@
+﻿function New-OUStructure {
+    [cmdletBinding()]
+    param(
+        [string] $CanonicalNameOU,
+        [System.Collections.IDictionary] $ConfigurationOU
+    )
+    $PartsOU = $CanonicalNameOU.Split("\")
+
+    $LevelPath = $BasePath
+    for ($i = 0; $i -lt $PartsOU.Length; $i++) {
+        $O = $PartsOU[$i]
+        $CurrentPath = 'OU=' + $O + ',' + $LevelPath
+
+        # lets create new OU, but if it exists we will just inform user
+        try {
+            $newADOrganizationalUnitSplat = @{
+                Name                            = $O
+                Path                            = $LevelPath
+                ProtectedFromAccidentalDeletion = $False
+                Server                          = $DC
+                ErrorAction                     = 'Stop'
+            }
+            if ($i -eq ($PartsOU.Count - 1)) {
+                foreach ($V in $ConfigurationOU.Keys) {
+                    $newADOrganizationalUnitSplat[$V] = $ConfigurationOU[$V]
+                }
+            }
+            Remove-EmptyValue -Hashtable $newADOrganizationalUnitSplat
+            New-ADOrganizationalUnit @newADOrganizationalUnitSplat
+            Write-Verbose -Message "New-OUStructure - Created new OU $CurrentPath"
+
+        } catch {
+            if ($_.Exception.Message -notlike '*with a name that is already in use*') {
+                Write-Warning -Message "New-OUStructure - Error: $($5_.Exception.message)"
+            } else {
+                Write-Verbose -Message "New-OUStructure - Skipped new OU $CurrentPath, already exists."
+            }
+        }
+        # once the OU is created we should update it with DEscription,
+        Write-Verbose -Message "New-OUStructure - Updating OU $CurrentPath"
+        $setADOrganizationalUnitSplat = @{
+            Identity    = $CurrentPath
+            Description = $Description
+            #ProtectedFromAccidentalDeletion = $ProtectedFromAccidentalDeletion
+        }
+
+        if ($i -eq ($PartsOU.Count - 1)) {
+            foreach ($V in $ConfigurationOU.Keys) {
+                $setADOrganizationalUnitSplat[$V] = $ConfigurationOU[$V]
+            }
+        }
+
+        Set-ADOrganizationalUnit @setADOrganizationalUnitSplat
+        $LevelPath = 'OU=' + $O + ',' + $LevelPath
+    }
+}
---
+++ Public/Start-DelegationModel.ps1
@@ -0,0 +1,15 @@
+﻿function Start-DelegationModel {
+    [cmdletBinding()]
+    param(
+        [Parameter(Mandatory)][string] $Domain,
+        [System.Collections.IDictionary] $Definition
+    )
+    $BasePath = ConvertTo-DistinguishedName -CanonicalName $Domain
+    if (-not $BasePath) {
+        return
+    }
+    foreach ($CanonicalNameOU in $Definition.Keys) {
+        $ConfigurationOU = $Definition[$CanonicalNameOU]
+        New-OUStructure -CanonicalNameOU $CanonicalNameOU -ConfigurationOU $ConfigurationOU
+    }
+}
