2a5acee43d5ff74e3a2fe8de06a253e499224ab3
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-12-18 19:00:52 +0100

    Add function to create delegation model

---
+++ Private/New-DelegationModel.ps1
@@ -0,0 +1,15 @@
+﻿function New-DelegationModel {
+    [cmdletBinding()]
+    param(
+        [string] $CanonicalNameOU,
+        [System.Collections.IDictionary] $ConfigurationOU,
+        [string] $BasePath,
+        [string] $Domain
+    )
+    $CanonicalNameOU = $CanonicalNameOU.Replace("\", "/")
+    $DNOU = ConvertTo-DistinguishedName -CanonicalName "$Domain/$($CanonicalNameOU)" -ToOU
+    if ($null -ne $ConfigurationOU.DelegationInheritance) {
+        Set-ADACLInheritance -ADObject $DNOU -Inheritance $ConfigurationOU.DelegationInheritance
+    }
+    Set-ADACL -ADObject $DNOU -ACLSettings $ConfigurationOU.Delegation -Inheritance $ConfigurationOU.DelegationInheritance
+}
