68dae92e92243354ca54a9c98259896856db9b72
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-01-05 20:15:38 +0100

    Moved group updates to different function

---
+++ Private/Repair-GroupData.ps1
@@ -0,0 +1,72 @@
+﻿function Repair-GroupData {
+    [CmdletBinding()]
+    param(
+        [string] $Group,
+        [Array]  $PropertiesChangable,
+        [Array] $StandardChangable,
+        [Microsoft.ActiveDirectory.Management.ADGroup] $GroupExists,
+        [System.Collections.IDictionary]$GroupObject,
+        [string] $DC
+    )
+    Write-Color -Text '[-] ', "Group ", $Group, " already exists" -Color Red, White, Yellow, White
+    foreach ($Key in $PropertiesChangable) {
+        # we need to check whether key is defined at all and user wants to update it
+        if ($null -ne $GroupObject.$Key -and $GroupExists.$Key -ne $GroupObject.$Key) {
+            try {
+                # we need to make sure DisplayName, Name are not empty, rest can be empty
+                if ($Key -in @('DisplayName', 'Name') -and -not $Key) {
+                    continue
+                }
+                if ($Key -in $StandardChangable) {
+                    $setADObjectSplat = @{
+                        Identity    = $GroupExists.DistinguishedName
+                        ErrorAction = 'Stop'
+                        $Key        = $GroupObject.$Key
+                        Server      = $DC
+                    }
+                    Set-ADObject @setADObjectSplat
+                } else {
+                    Set-ADGroup -Identity $Group -Replace @{ $Key = $GroupObject.$Key } -ErrorAction Stop -Server $DC
+                }
+                Write-Color -Text '[+] ', "Group ", $Group, " ", $Key, " updated" -Color Green, White, Green, White, Green, White
+            } catch {
+                Write-Color -Text '[-] ', "Group ", $Group, " ", $Key, " update failed. Error: ", $_.Exception.Message -Color Red, White, Red, White, Red
+            }
+        }
+    }
+    $Location = ConvertFrom-DistinguishedName -DistinguishedName $GroupExists.DistinguishedName -ToOrganizationalUnit
+    if ($Location -ne $GroupObject.Path) {
+        if ($GroupExists.ProtectedFromAccidentalDeletion) {
+            $ProtectedFromAccidentalDeletionFailed = $false
+            try {
+                Set-ADObject -ProtectedFromAccidentalDeletion $false -Identity $GroupExists.DistinguishedName -ErrorAction Stop -Server $DC
+            } catch {
+                Write-Color -Text '[-] ', "Group ", $Group, " move to ", $GroupObject.Path, " failed. Couldn't disable ProtectedFromAccidentalDeletion. Error: ", $_.Exception.Message -Color Red, White, Red, White, Red
+                $ProtectedFromAccidentalDeletionFailed = true
+            }
+        }
+        if (-not $ProtectedFromAccidentalDeletionFailed) {
+            $MoveFailed = $false
+            try {
+                $Test = Move-ADObject -Identity $GroupExists.DistinguishedName -TargetPath $GroupObject.Path -ErrorAction Stop -Server $DC
+                Write-Color -Text '[+] ', "Group ", $Group, " moved to ", $GroupObject.Path -Color Green, White, Green, White
+            } catch {
+                $MoveFailed = $true
+                Write-Color -Text '[-] ', "Group ", $Group, " move to ", $GroupObject.Path, " failed. Error: ", $_.Exception.Message -Color Red, White, Red, White, Red
+            }
+            if (-not $MoveFailed) {
+                $PathToGroup = $GroupObject.Path
+            } else {
+                $PathToGroup = $GroupExists.DistinguishedName
+            }
+            if ($GroupExists.ProtectedFromAccidentalDeletion) {
+                try {
+                    Set-ADObject -ProtectedFromAccidentalDeletion $true -Identity $PathToGroup -ErrorAction Stop -Server $DC
+                } catch {
+                    Write-Color -Text '[-] ', "Group ", $Group, " move to ", $GroupObject.Path, " failed (maybe?). Couldn't enable ProtectedFromAccidentalDeletion. Error: ", $_.Exception.Message -Color Red, White, Red, White, Red
+                }
+            }
+
+        }
+    }
+}
