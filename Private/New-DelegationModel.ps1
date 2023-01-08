1cbcb96d6d418295d75a604a4015b8f262d11e63
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-01-08 15:44:02 +0100

    Improve error handling

--- Private/New-DelegationModel.ps1
+++ Private/New-DelegationModel.ps1
@@ -9,7 +9,13 @@
     $CanonicalNameOU = $CanonicalNameOU.Replace("\", "/")
     $DNOU = ConvertTo-DistinguishedName -CanonicalName "$Domain/$($CanonicalNameOU)" -ToOU
     if ($null -ne $ConfigurationOU.DelegationInheritance) {
-        Set-ADACLInheritance -ADObject $DNOU -Inheritance $ConfigurationOU.DelegationInheritance
+        Set-ADACLInheritance -ADObject $DNOU -Inheritance $ConfigurationOU.DelegationInheritance -WarningAction SilentlyContinue -ErrorVariable errors -ErrorAction SilentlyContinue -WarningVariable warnings
+        foreach ($W in $Warnings) {
+            Write-Color -Text "[!]", "[$CanonicalNameOU]", "[Warning]", " ACL Inheritance: $($W.Exception.Message)" -Color DarkMagenta, DarkGray, DarkMagenta, White
+        }
+        foreach ($E in $Errors) {
+            Write-Color -Text "[!]", "[$CanonicalNameOU]", "[Error]", " ACL Inheritance: $($E.Exception.Message)" -Color Red, DarkGray, Red, White
+        }
     }
     Set-ADACL -ADObject $DNOU -ACLSettings $ConfigurationOU.Delegation -Inheritance $ConfigurationOU.DelegationInheritance -WarningAction SilentlyContinue
 }
