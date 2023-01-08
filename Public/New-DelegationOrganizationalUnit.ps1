c57a0cbd156295ed37555784794324837482035c
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-01-08 11:34:17 +0100

    Update delegation OU

---
+++ Public/New-DelegationOrganizationalUnit.ps1
@@ -0,0 +1,21 @@
+﻿function New-DelegationOrganizationalUnit {
+    [alias('New-DelegationOU')]
+    [CmdletBinding()]
+    param(
+        [parameter(Mandatory)][string] $CanonicalNameOU,
+        [string] $Description,
+        [ValidateSet('Enabled', 'Disabled')][string] $DelegationInheritance,
+        [Array] $Delegation,
+        [bool] $ProtectedFromAccidentalDeletion
+    )
+
+    $InputData = [ordered] @{
+        CanonicalNameOU                 = $CanonicalNameOU
+        Description                     = $Description
+        DelegationInheritance           = $DelegationInheritance
+        Delegation                      = $Delegation
+        ProtectedFromAccidentalDeletion = If ($PSBoundParameters.ContainsKey('ProtectedFromAccidentalDeletion')) { $ProtectedFromAccidentalDeletion } else { $null }
+    }
+    Remove-EmptyValue -Hashtable $InputData
+    $InputData
+}
