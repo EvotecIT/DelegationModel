2c5ddc5e5b3dcd14973b80f9c9b640a0d004632c
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-01-09 12:59:25 +0100

    Improved examples

--- Examples/01-CreateGroups02.ps1
+++ Examples/01-CreateGroups02.ps1
@@ -1,7 +1,7 @@
 ﻿Import-Module .\DelegationModel.psd1 -Force
 
 # Similar to Examples\01-CreateGroups01.ps1 - Alternative way to create/manage groups
-$AdministrativeUnits = 'FR_IT_Team08', 'FR_IT_Team09'
+$AdministrativeUnits = 'FR_IT_Team08', 'FR_IT_Team09', 'FR_IT_Team06'
 $DomainDN = "DC=ad,DC=evotec,DC=xyz"
 $OUPath = "OU=Administration_Tasks_Groups,OU=Administration,$DomainDN"
 
@@ -19,14 +19,25 @@ Start-DelegationGroups -MembersBehaviour 'Add', 'Remove' -Destination 'OU=Delega
         New-DelegationGroup -Name "DL_Tier2_Computers_B_$AdministrativeUnit" -Path $OUPath -GroupScope 'DomainLocal' -GroupCategory 'Security'
         New-DelegationGroup -Name "DL_Tier2_Devices_$AdministrativeUnit" -Path $OUPath -GroupScope 'DomainLocal' -GroupCategory 'Security'
         New-DelegationGroup -Name "DL_Tier2_LAPS_B_Administrator_$AdministrativeUnit" -Path $OUPath -GroupScope 'DomainLocal' -GroupCategory 'Security'
+        New-DelegationGroup -Name "DL_Tier2_OU_$AdministrativeUnit" -Path $OUPath -GroupScope DomainLocal -GroupCategory Security
+        New-DelegationGroup -Name "DL_Tier2_UserVIP_$AdministrativeUnit" -Path $OUPath -GroupScope DomainLocal -GroupCategory Security
+        New-DelegationGroup -Name "DL_Tier2_User_$AdministrativeUnit" -Path $OUPath -GroupScope DomainLocal -GroupCategory Security
+        New-DelegationGroup -Name "DL_Tier2_Groups_$AdministrativeUnit" -Path $OUPath -GroupScope DomainLocal -GroupCategory Security
+        New-DelegationGroup -Name "DL_Tier2_RSOP_$AdministrativeUnit" -Path $OUPath -GroupScope DomainLocal -GroupCategory Security
+        New-DelegationGroup -Name "DL_Tier2_PWDMgmt_$AdministrativeUnit" -Path $OUPath -GroupScope DomainLocal -GroupCategory Security
+        New-DelegationGroup -Name "DL_Tier2_DevicesLAPSRead_$AdministrativeUnit" -Path $OUPath -GroupScope DomainLocal -GroupCategory Security
+        New-DelegationGroup -Name "DL_Tier2_DevicesVIPLAPSRead_$AdministrativeUnit" -Path $OUPath -GroupScope DomainLocal -GroupCategory Security
+        New-DelegationGroup -Name "DL_Tier2_DevicesVIP_$AdministrativeUnit" -Path $OUPath -GroupScope DomainLocal -GroupCategory Security
+        New-DelegationGroup -Name "DL_Tier2_Instruments_$AdministrativeUnit" -Path $OUPath -GroupScope DomainLocal -GroupCategory Security
+        New-DelegationGroup -Name "DL_Tier2_InstrumentsLAPSRead_$AdministrativeUnit" -Path $OUPath -GroupScope DomainLocal -GroupCategory Security
         New-DelegationGroup -Name "GG_Tier2_Service_Desk_Admins" -Path $OUPath -GroupScope 'DomainLocal' -GroupCategory 'Security' -Members @(
-
+            # add members if needed
         ) -MemberOf @(
             "DL_Tier2_User_Account_B_$AdministrativeUnit"
             "DL_Tier2_Groups_B_$AdministrativeUnit"
         )
         New-DelegationGroup -Name "GG_Tier2_LDAP_Admins_$AdministrativeUnit" -Path $OUPath -GroupScope 'DomainLocal' -GroupCategory 'Security' -Members @(
-
+            # add members if needed
         ) -MemberOf @(
             "DL_Tier2_LAPS_A_Administrator_$AdministrativeUnit"
         )
@@ -49,4 +60,4 @@ Start-DelegationGroups -MembersBehaviour 'Add', 'Remove' -Destination 'OU=Delega
         }
         New-DelegationGroup @newDelegationGroupSplat -MembersBehaviour 'Add', 'Remove'
     }
-}
+} -LogOption Add, Remove
