436cc7e67abfe7821d9eefea0f07bd4aba114ad4
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-01-09 13:00:40 +0100

    Added logging options

--- Public/Start-DelegationModel.ps1
+++ Public/Start-DelegationModel.ps1
@@ -5,11 +5,11 @@
         [Parameter(Mandatory)][string] $Domain,
         [System.Collections.IDictionary] $Definition,
         [bool] $ProtectedFromAccidentalDeletion,
-        [switch] $ObjectOutput,
+        [switch] $DontSuppress,
         [string] $LogFile,
-        [int] $LogMaximum = 60
+        [int] $LogMaximum = 60,
+        [ValidateSet('Add', 'Remove', 'Skip')][string[]] $LogOption = @('Add', 'Skip', 'Remove')
     )
-
     $Script:Cache = [ordered] @{}
     $Script:Reporting = [ordered] @{}
     $Script:Reporting['Version'] = Get-GitHubVersion -Cmdlet 'Start-DelegationModel' -RepositoryOwner 'evotecit' -RepositoryName 'DelegationModel'
@@ -56,9 +56,21 @@
 
     $DC = $ForestInformation['QueryServers'][$Domain].HostName[0]
 
+    # lets reset the cache of users and groups
+    # this is required since we often just created groups in the same run
+    $null = New-ADACLObject -Principal 'S-1-5-11' -AccessControlType Allow -ObjectType All -InheritedObjectTypeName All -AccessRule GenericAll -InheritanceType None -Force
+
     Write-Color '[i]', "[DelegationModel] ", 'Preparing data to be configured' -Color Yellow, DarkGray, Yellow
     if ($PSBoundParameters.ContainsKey('DelegationModelDefinition')) {
-        $DelegationInput = Invoke-Command -ScriptBlock $DelegationModelDefinition -Verbose
+        $DelegationInput = Invoke-Command -ScriptBlock $DelegationModelDefinition -Verbose -WarningAction SilentlyContinue -WarningVariable Warnings
+        #$DelegationInput = Invoke-CommandCustom -ScriptBlock $DelegationModelDefinition -ReturnVerbose -ReturnError -ReturnWarning
+        # foreach ($W in $DelegationInput.Warning) {
+        #     Write-Color -Text "[!]", "[Delegationmodel]", "[Warning]", " Preloading rules, $($W)" -Color DarkMagenta, DarkGray, DarkMagenta, White
+        # }
+        # foreach ($W in $DelegationInput.Error) {
+        #     Write-Color -Text "[!]", "[Delegationmodel]", "[Error]", " Preloading rules, $($W)" -Color DarkMagenta, DarkGray, DarkMagenta, White
+        # }
+        #$Definition = Convert-DelegationModel -DelegationInput $DelegationInput.Output -Destination $Destination -ProtectedFromAccidentalDeletion $ProtectedFromAccidentalDeletion
         $Definition = Convert-DelegationModel -DelegationInput $DelegationInput -Destination $Destination -ProtectedFromAccidentalDeletion $ProtectedFromAccidentalDeletion
     } else {
         $Definition = Convert-DelegationModel -Definition $Definition -Destination $Destination -ProtectedFromAccidentalDeletion $ProtectedFromAccidentalDeletion
@@ -74,8 +86,8 @@
         $ConfigurationOU = $Definition[$CanonicalNameOU]
         if ($ConfigurationOU.Delegation) {
             $OutputFromDelegation = New-DelegationModel -Domain $Domain -CanonicalNameOU $CanonicalNameOU -ConfigurationOU $ConfigurationOU -BasePath $BasePath
-            Export-DelegationLogs -OutputFromDelegation $OutputFromDelegation -CanonicalNameOU $CanonicalNameOU
-            if ($ObjectOutput) {
+            Export-DelegationLogs -LogOption $LogOption -OutputFromDelegation $OutputFromDelegation -CanonicalNameOU $CanonicalNameOU
+            if ($DontSuppress) {
                 $OutputFromDelegation
             }
         }
