2ad869f52cf535fea7e724cf9c1afa8a05b73f6d
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-01-09 13:00:24 +0100

    Added logging options Prevent code from being executed

--- Private/New-OUStructure.ps1
+++ Private/New-OUStructure.ps1
@@ -15,6 +15,10 @@
         $O = $PartsOU[$i]
         $CurrentPath = 'OU=' + $O + ',' + $LevelPath
         $CanonicalCurrentPath = ConvertFrom-DistinguishedName -DistinguishedName ($CurrentPath.Replace(",$BasePath", "")) -ToCanonicalName
+        $DirectRequest = $false
+        if ($i -eq ($PartsOU.Count - 1)) {
+            $DirectRequest = $true
+        }
 
         # lets create new OU, but if it exists we will just inform user
         $OrganizationalUnitExists = Get-ADOrganizationalUnit -Filter "DistinguishedName -eq '$CurrentPath'" -ErrorAction SilentlyContinue -Properties $OUProperties -Server $DC
@@ -27,7 +31,7 @@
                     Server                          = $DC
                     ErrorAction                     = 'Stop'
                 }
-                if ($i -eq ($PartsOU.Count - 1)) {
+                if ($DirectRequest) {
                     foreach ($V in $ConfigurationOU.Keys) {
                         if ($V -notin $IgnoredProperties) {
                             $newADOrganizationalUnitSplat[$V] = $ConfigurationOU[$V]
@@ -45,20 +49,24 @@
                 if ($_.Exception.Message -notlike '*with a name that is already in use*') {
                     Write-Color -Text '[!]', "[$CanonicalCurrentPath]", "[Error]", " Error $($_.Exception.message)" -Color Red, DarkGray, Yellow, DarkMagenta, White
                 } else {
-                    Write-Color -Text '[*]', "[$CanonicalCurrentPath]", "[Skipping]", " Skipped new organizational unit, already exists!" -Color DarkMagenta, DarkGray, Yellow, DarkMagenta, White
+                    if ($DirectRequest) {
+                        Write-Color -Text '[*]', "[$CanonicalCurrentPath]", "[Skipping]", " Skipped new organizational unit, already exists!" -Color DarkMagenta, DarkGray, Yellow, DarkMagenta, White
+                    }
                 }
             }
         } else {
-            Write-Color -Text '[*]', "[$CanonicalCurrentPath]", "[Skipping]", " Skipped new organizational unit, already exists!" -Color DarkMagenta, DarkGray, Yellow, DarkMagenta, White
+            if ($DirectRequest) {
+                Write-Color -Text '[*]', "[$CanonicalCurrentPath]", "[Skipping]", " Skipped new organizational unit, already exists!" -Color DarkMagenta, DarkGray, Yellow, DarkMagenta, White
+            }
         }
-        # once the OU is created we should update it with additional fields
-        $setADOrganizationalUnitSplat = @{
-            Identity = $CurrentPath
-            Server   = $DC
-        }
-
-        $PropertiesToUpdate = if ($i -eq ($PartsOU.Count - 1)) {
-            foreach ($V in $ConfigurationOU.Keys) {
+        if ($DirectRequest) {
+            # lets update only if it's direct request, and not just a parent OU that was created before
+            # once the OU is created we should update it with additional fields
+            $setADOrganizationalUnitSplat = @{
+                Identity = $CurrentPath
+                Server   = $DC
+            }
+            $PropertiesToUpdate = foreach ($V in $ConfigurationOU.Keys) {
                 if ($V -notin $IgnoredProperties) {
                     if ($OrganizationalUnitExists.$V -ne $ConfigurationOU[$V]) {
                         $V
@@ -66,12 +74,12 @@
                     }
                 }
             }
-        }
-        if ($setADOrganizationalUnitSplat.Count -eq 2) {
-            #Write-Color -Text '[!] ', "Nothing to update for OU ", $CurrentPath -Color Red, White
-        } else {
-            Write-Color -Text '[+]', "[$CanonicalCurrentPath]", "[Updating]", " Updating organizational unit with fields: ", ($PropertiesToUpdate -join ", ") -Color Green, DarkGray, Green, White
-            Set-ADOrganizationalUnit @setADOrganizationalUnitSplat
+            if ($setADOrganizationalUnitSplat.Count -eq 2) {
+                #Write-Color -Text '[!] ', "Nothing to update for OU ", $CurrentPath -Color Red, White
+            } else {
+                Write-Color -Text '[+]', "[$CanonicalCurrentPath]", "[Updating]", " Updating organizational unit with fields: ", ($PropertiesToUpdate -join ", ") -Color Green, DarkGray, Green, White
+                Set-ADOrganizationalUnit @setADOrganizationalUnitSplat
+            }
         }
         $LevelPath = 'OU=' + $O + ',' + $LevelPath
     }
