03b51e35ae5749dda7e3b75fdfd97c8844aabe03
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-01-09 13:00:50 +0100

    Added logging options

--- Public/Start-DelegationGroups.ps1
+++ Public/Start-DelegationGroups.ps1
@@ -6,7 +6,8 @@
         [Parameter(Mandatory)][string] $Domain,
         [System.Collections.IDictionary] $Groups,
         [string[]][ValidateSet('Add', 'Remove')] $MembersBehaviour,
-        [bool] $ProtectedFromAccidentalDeletion
+        [bool] $ProtectedFromAccidentalDeletion,
+        [ValidateSet('Add', 'Remove', 'Skip')][string[]] $LogOption = @('Add', 'Skip', 'Remove')
     )
     $Properties = @('Name', 'Description', 'DisplayName', 'GroupScope', 'GroupCategory', 'ProtectedFromAccidentalDeletion')
     $PropertiesChangable = @('Description', 'DisplayName', 'ProtectedFromAccidentalDeletion', 'Path')
@@ -18,7 +19,7 @@
     }
     $ForestInformation = Get-WinADForestDetails
     if (-not $ForestInformation) {
-        Write-Color -Text '[-] ', "Forest information could not be retrieved. Please check your connection to the domain controller." -Color Red, White
+        Write-Color -Text '[!] ', "Forest information could not be retrieved. Please check your connection to the domain controller." -Color Red, White
         return
     }
     $DC = $ForestInformation['QueryServers'][$Domain].HostName[0]
@@ -39,7 +40,7 @@
                 $null = Get-ADOrganizationalUnit -Identity $GroupObject.Path -ErrorAction Stop -Server $DC
             } catch {
                 $OUCheck = $false
-                Write-Color -Text '[-] ', "Path OU $($GroupObject.Path)", " verification failed. Please create all organizational Units before continuing. Error: ", $_.Exception.Message -Color Red, Yellow, White
+                Write-Color -Text '[!] ', "Path OU $($GroupObject.Path)", " verification failed. Please create all organizational Units before continuing. Error: ", $_.Exception.Message -Color Red, Yellow, White
             }
         }
     }
@@ -63,19 +64,21 @@
                 # if the group does not exist, we will create it
                 try {
                     $null = New-ADGroup @newADGroupSplat -ErrorAction Stop
-                    Write-Color -Text '[+] ', "Group ", $Group, " created" -Color Green, White, Green, White
+                    if ($LogOption -contains 'Add') {
+                        Write-Color -Text '[+] ', "Group ", $Group, " created" -Color Green, White, Green, White
+                    }
                 } catch {
-                    Write-Color -Text '[-] ', "Group ", $Group, " creation failed. Error: ", $_.Exception.Message -Color Red, White, Red, White
+                    Write-Color -Text '[!] ', "Group ", $Group, " creation failed. Error: ", $_.Exception.Message -Color Red, White, Red, White
                 }
             } else {
                 # if the group exists, we will check whether it needs to be updated
-                Repair-GroupData -Group $Group -PropertiesChangable $PropertiesChangable -StandardChangable $StandardChangable -GroupObject $GroupObject -GroupExists $GroupExists -DC $DC
+                Repair-GroupData -Group $Group -PropertiesChangable $PropertiesChangable -StandardChangable $StandardChangable -GroupObject $GroupObject -GroupExists $GroupExists -DC $DC -LogOption $LogOption
             }
         }
         Write-Color -Text "[i] ", "Processing Members for groups" -Color DarkBlue, White
         foreach ($Group in $Groups.Keys) {
             if ($null -ne $Groups[$Group].Members) {
-                Find-GroupMembersActions -Identity $Group -ExpectedMembers $Groups[$Group].Members -DC $DC -MembersBehaviour $Groups[$Group].MembersBehaviour
+                Find-GroupMembersActions -Identity $Group -ExpectedMembers $Groups[$Group].Members -DC $DC -MembersBehaviour $Groups[$Group].MembersBehaviour -LogOption $LogOption
             }
         }
         # MemberOf we will not attempt to cleanup as this may be some special group
@@ -83,7 +86,7 @@
         foreach ($Group in $Groups.Keys) {
             if ($null -ne $Groups[$Group].MemberOf) {
                 foreach ($MemberOf in $Groups[$Group].MemberOf) {
-                    Add-GroupMembersOf -Identity $MemberOf -Group $Group -DC $DC
+                    Add-GroupMembersOf -Identity $MemberOf -Group $Group -DC $DC -LogOption $LogOption
                 }
             }
         }
