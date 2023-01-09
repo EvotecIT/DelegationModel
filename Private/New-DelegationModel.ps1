b8e72ec219bef55c1501bf8033ba0c8223b8628c
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-01-09 13:00:04 +0100

    Added logging options

--- Private/New-DelegationModel.ps1
+++ Private/New-DelegationModel.ps1
@@ -9,13 +9,13 @@
     $CanonicalNameOU = $CanonicalNameOU.Replace("\", "/")
     $DNOU = ConvertTo-DistinguishedName -CanonicalName "$Domain/$($CanonicalNameOU)" -ToOU
     if ($null -ne $ConfigurationOU.DelegationInheritance) {
-        Set-ADACLInheritance -ADObject $DNOU -Inheritance $ConfigurationOU.DelegationInheritance -WarningAction SilentlyContinue -ErrorVariable errors -ErrorAction SilentlyContinue -WarningVariable warnings
+        Set-ADACLInheritance -ADObject $DNOU -Inheritance $ConfigurationOU.DelegationInheritance -WarningAction SilentlyContinue -WarningVariable warnings #-ErrorVariable errors -ErrorAction SilentlyContinue
         foreach ($W in $Warnings) {
-            Write-Color -Text "[!]", "[$CanonicalNameOU]", "[Warning]", " ACL Inheritance: $($W.Exception.Message)" -Color DarkMagenta, DarkGray, DarkMagenta, White
+            Write-Color -Text "[!]", "[$CanonicalNameOU]", "[Warning]", " ACL Inheritance: $($W)" -Color DarkMagenta, DarkGray, DarkMagenta, White
         }
-        foreach ($E in $Errors) {
-            Write-Color -Text "[!]", "[$CanonicalNameOU]", "[Error]", " ACL Inheritance: $($E.Exception.Message)" -Color Red, DarkGray, Red, White
-        }
+        # foreach ($E in $Errors) {
+        #     Write-Color -Text "[!]", "[$CanonicalNameOU]", "[Error]", " ACL Inheritance: $($E.Exception.Message)" -Color Red, DarkGray, Red, White
+        # }
     }
     Set-ADACL -ADObject $DNOU -ACLSettings $ConfigurationOU.Delegation -Inheritance $ConfigurationOU.DelegationInheritance -WarningAction SilentlyContinue
 }
