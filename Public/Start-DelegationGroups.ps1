9220cbdf7a2a5daa0003fba3a159abadc0007364
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-01-24 17:22:09 +0100

    Improve output

--- Public/Start-DelegationGroups.ps1
+++ Public/Start-DelegationGroups.ps1
@@ -9,6 +9,7 @@
         [bool] $ProtectedFromAccidentalDeletion,
         [ValidateSet('Add', 'Remove', 'Skip')][string[]] $LogOption = @('Add', 'Skip', 'Remove')
     )
+
     $Properties = @('Name', 'Description', 'DisplayName', 'GroupScope', 'GroupCategory', 'ProtectedFromAccidentalDeletion')
     $PropertiesChangable = @('Description', 'DisplayName', 'ProtectedFromAccidentalDeletion', 'Path')
     $StandardChangable = @("GroupCategory", "GroupScope", "Name", "Path", "ProtectedFromAccidentalDeletion")
@@ -17,12 +18,12 @@
     if (-not $BasePath) {
         return
     }
-    $ForestInformation = Get-WinADForestDetails
-    if (-not $ForestInformation) {
-        Write-Color -Text '[!] ', "Forest information could not be retrieved. Please check your connection to the domain controller." -Color Red, White
+
+    # Initialize Delegation Model
+    $DC = Initialize-DelegationModel -Domain $Domain
+    if (-not $DC) {
         return
     }
-    $DC = $ForestInformation['QueryServers'][$Domain].HostName[0]
 
     if ($PSBoundParameters.ContainsKey('DelegationGroupsDefinition')) {
         $DelegationOutput = & $DelegationGroupsDefinition
@@ -46,7 +47,7 @@
     }
     if ($OUCheck) {
         # lets fix the groups
-        Write-Color -Text "[i] ", "Processing creation of groups" -Color DarkBlue, White
+        Write-Color -Text "[i] ", "Processing creation of groups" -Color Cyan, White
         foreach ($Group in $Groups.Keys) {
             $GroupObject = $Groups[$Group]
             $newADGroupSplat = @{
@@ -59,34 +60,36 @@
                 DisplayName   = if ($GroupObject.DisplayName) { $GroupObject.DisplayName } else { $Group }
             }
             Remove-EmptyValue -Hashtable $newADGroupSplat
-            $GroupExists = Get-ADGroup -Filter "Name -eq '$Group'" -Properties $Properties -Server $DC
+            $GroupExists = Get-ADGroup -Filter "Name -eq '$($GroupObject.Name)'" -Properties $Properties -Server $DC
             if (-not $GroupExists) {
                 # if the group does not exist, we will create it
                 try {
                     $null = New-ADGroup @newADGroupSplat -ErrorAction Stop
                     if ($LogOption -contains 'Add') {
-                        Write-Color -Text '[+] ', "Group ", $Group, " created" -Color Green, White, Green, White
+                        Write-Color -Text '[+] ', "Group ", $GroupObject.Name, " created" -Color Green, White, Green, White
                     }
                 } catch {
-                    Write-Color -Text '[!] ', "Group ", $Group, " creation failed. Error: ", $_.Exception.Message -Color Red, White, Red, White
+                    Write-Color -Text '[!] ', "Group ", $GroupObject.Name, " creation failed. Error: ", $_.Exception.Message -Color Red, White, Red, White
                 }
             } else {
                 # if the group exists, we will check whether it needs to be updated
-                Repair-GroupData -Group $Group -PropertiesChangable $PropertiesChangable -StandardChangable $StandardChangable -GroupObject $GroupObject -GroupExists $GroupExists -DC $DC -LogOption $LogOption
+                Repair-GroupData -Group $GroupObject.Name -PropertiesChangable $PropertiesChangable -StandardChangable $StandardChangable -GroupObject $GroupObject -GroupExists $GroupExists -DC $DC -LogOption $LogOption
             }
         }
-        Write-Color -Text "[i] ", "Processing Members for groups" -Color DarkBlue, White
+        Write-Color -Text "[i] ", "Processing Members for groups" -Color Cyan, White
         foreach ($Group in $Groups.Keys) {
+            $GroupObject = $Groups[$Group]
             if ($null -ne $Groups[$Group].Members) {
-                Find-GroupMembersActions -Identity $Group -ExpectedMembers $Groups[$Group].Members -DC $DC -MembersBehaviour $Groups[$Group].MembersBehaviour -LogOption $LogOption
+                Find-GroupMembersActions -Identity $GroupObject.Name -ExpectedMembers $Groups[$Group].Members -DC $DC -MembersBehaviour $Groups[$Group].MembersBehaviour -LogOption $LogOption
             }
         }
         # MemberOf we will not attempt to cleanup as this may be some special group
-        Write-Color -Text "[i] ", "Processing MemberOf for groups" -Color DarkBlue, White
+        Write-Color -Text "[i] ", "Processing MemberOf for groups" -Color Cyan, White
         foreach ($Group in $Groups.Keys) {
+            $GroupObject = $Groups[$Group]
             if ($null -ne $Groups[$Group].MemberOf) {
                 foreach ($MemberOf in $Groups[$Group].MemberOf) {
-                    Add-GroupMembersOf -Identity $MemberOf -Group $Group -DC $DC -LogOption $LogOption
+                    Add-GroupMembersOf -Identity $MemberOf -Group $GroupObject.Name -DC $DC -LogOption $LogOption
                 }
             }
         }
--- Public/Start-DelegationModel.ps1
+++ Public/Start-DelegationModel.ps1
@@ -11,51 +11,19 @@
         [ValidateSet('Add', 'Remove', 'Skip')][string[]] $LogOption = @('Add', 'Skip', 'Remove')
     )
     $Script:Cache = [ordered] @{}
-    $Script:Reporting = [ordered] @{}
-    $Script:Reporting['Version'] = Get-GitHubVersion -Cmdlet 'Start-DelegationModel' -RepositoryOwner 'evotecit' -RepositoryName 'DelegationModel'
-    $Script:Reporting['Settings'] = @{
-        ShowError   = $ShowError.IsPresent
-        ShowWarning = $ShowWarning.IsPresent
-        HideSteps   = $HideSteps.IsPresent
-    }
 
-    if ($LogFile) {
-        $FolderPath = [io.path]::GetDirectoryName($LogFile)
-        if (-not (Test-Path -LiteralPath $FolderPath)) {
-            $null = New-Item -Path $FolderPath -ItemType Directory -Force -WhatIf:$false
-        }
-        $PSDefaultParameterValues = @{
-            "Write-Color:LogFile" = $LogFile
-        }
-        Write-Color '[i]', "[DelegationModel] ", 'Version', $Script:Reporting['Version'] -Color Yellow, DarkGray, Yellow, DarkGray, Magenta
-        $CurrentLogs = Get-ChildItem -LiteralPath $FolderPath | Sort-Object -Property CreationTime -Descending | Select-Object -Skip $LogMaximum
-        if ($CurrentLogs) {
-            Write-Color -Text '[i] ', "[DelegationModel] ", "Logs directory has more than ", $LogMaximum, " log files. Cleanup required..." -Color Yellow, DarkCyan, Red, DarkCyan
-            foreach ($Log in $CurrentLogs) {
-                try {
-                    Remove-Item -LiteralPath $Log.FullName -Confirm:$false -WhatIf:$false
-                    Write-Color -Text '[i] ', "[DelegationModel] ", '[log deleted] ', "Deleted ", "$($Log.FullName)" -Color Yellow, White, Green
-                } catch {
-                    Write-Color -Text '[i] ', "[DelegationModel] ", '[log error] ', "Couldn't delete log file $($Log.FullName). Error: ', "$($_.Exception.Message) -Color Yellow, White, Red
-                }
-            }
-        }
-    } else {
-        Write-Color '[i]', "[DelegationModel] ", 'Version', $Script:Reporting['Version'] -Color Yellow, DarkGray, Yellow, DarkGray, Magenta
-    }
     $BasePath = ConvertTo-DistinguishedName -CanonicalName $Domain
     if (-not $BasePath) {
         return
     }
-    Write-Color '[i]', "[DelegationModel] ", 'Getting forest information' -Color Yellow, DarkGray, Yellow
-    $ForestInformation = Get-WinADForestDetails
-    if (-not $ForestInformation) {
-        Write-Color -Text '[-] ', "Forest information could not be retrieved. Please check your connection to the domain controller." -Color Red, White
+
+    # Initialize Delegation Model
+    $DC = Initialize-DelegationModel -Domain $Domain
+    if (-not $DC) {
         return
     }
 
-    $DC = $ForestInformation['QueryServers'][$Domain].HostName[0]
-
+    Write-Color -Text '[i]', "[DelegationModel] ", 'Domain Controller ', $DC -Color Yellow, DarkGray, Yellow, DarkGray, Magenta
     # lets reset the cache of users and groups
     # this is required since we often just created groups in the same run
     $null = New-ADACLObject -Principal 'S-1-5-11' -AccessControlType Allow -ObjectType All -InheritedObjectTypeName All -AccessRule GenericAll -InheritanceType None -Force
@@ -65,10 +33,10 @@
         $DelegationInput = Invoke-Command -ScriptBlock $DelegationModelDefinition -Verbose -WarningAction SilentlyContinue -WarningVariable Warnings
         #$DelegationInput = Invoke-CommandCustom -ScriptBlock $DelegationModelDefinition -ReturnVerbose -ReturnError -ReturnWarning
         # foreach ($W in $DelegationInput.Warning) {
-        #     Write-Color -Text "[!]", "[Delegationmodel]", "[Warning]", " Preloading rules, $($W)" -Color DarkMagenta, DarkGray, DarkMagenta, White
+        #     Write-Color -Text "[!]", "[Delegationmodel]", "[Warning]", " Preloading rules, $($W)" -Color Magenta, DarkGray, Magenta, White
         # }
         # foreach ($W in $DelegationInput.Error) {
-        #     Write-Color -Text "[!]", "[Delegationmodel]", "[Error]", " Preloading rules, $($W)" -Color DarkMagenta, DarkGray, DarkMagenta, White
+        #     Write-Color -Text "[!]", "[Delegationmodel]", "[Error]", " Preloading rules, $($W)" -Color Magenta, DarkGray, Magenta, White
         # }
         #$Definition = Convert-DelegationModel -DelegationInput $DelegationInput.Output -Destination $Destination -ProtectedFromAccidentalDeletion $ProtectedFromAccidentalDeletion
         $Definition = Convert-DelegationModel -DelegationInput $DelegationInput -Destination $Destination -ProtectedFromAccidentalDeletion $ProtectedFromAccidentalDeletion
